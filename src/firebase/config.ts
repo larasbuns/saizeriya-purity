51f116cbb8cb759b7ac2ad95a53a5fc6d8055601
Lara Wong <larawongfengyi@gmail.com>
2025-12-18 14:00:51 +0000

    Try fixing this error: `Build Error: Export firebaseConfig doesn't exist

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyD-Jc0_Ng8apXjY8s8oPkRhvhP9MR71EVw",
   authDomain: "studio-1753067186-df39f.firebaseapp.com",
@@ -6,3 +12,6 @@ export const firebaseConfig = {
   messagingSenderId: "885022348288",
   appId: "1:885022348288:web:4197578e72c4251e6334e5"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
